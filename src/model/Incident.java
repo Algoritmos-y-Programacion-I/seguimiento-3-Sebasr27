d12d12e7e99a83e49e1a191d9b1b32f36fdba493
Sebasr27 <sebasrangulo@gmail.com>
2025-10-24 17:22:21 -0500

    commit 1

--- src/model/Incident.java
+++ src/model/Incident.java
@@ -1,5 +1,57 @@
 package model;
 
+
+import java.time.LocalDate;
 public class Incident {
+
+    private LocalDate dateReport;
+    private String description;
+    private boolean solution;
+    private int solutionHours;
+
+
+    public Incident (LocalDate dateReport, String description){
+
+        this.DateReport = dateReport;
+        this.description = description;
+        this.solution = false;
+        this.solutionHours = 0;
+
+    }
+
+    public LocalDate getDateReport() {
+        return DateReport;
+    }
     
+    public void setDateReport(LocalDate dateReport){
+        this.dateReport = dateReport;
+    }
+
+    public String getDescrition(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public boolean getSolution(){
+        return solution;
+    }
+
+    public void setSolution(boolean solution){
+        this.solution = solution;
+    }
+
+    public int getSolutionHours(){
+        return solutionHours;
+    }
+
+    public void setSolutionHours(int solutionHours){
+        this.solutionHours = solutionHours;
+    }
+
 }
+
+    
+
--- src/model/SchoolController.java
+++ src/model/SchoolController.java
@@ -1,6 +1,33 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Arraylist;
+
 public class SchoolController {
+    private String name;
+    private int hoursSpentSupport;
+
+    public static final int FLOORS = 5;
+    public static final int COL = 10;
+    public static final int HOURMAXSUPPORT = 100;
+
+    private Computer [][] computerMatrix;
+
+    public SchoolController(String name) {
+        this.name = name;
+        this.hoursSpentSupport = 0;
+        computerMatrix = new Computer[FLOORS][COL];
+    }
+
+    public String addComputer(String serial, boolean nextWindow, int floor, int column){
+        if (computerMatrix [¨floor][Column] != mull) {
+            return "YA EXISTE UN COMPUTADOR EN ESA POSICION.";
+
+        }
+
+
+    }
+
 
     /*
      * ATENCION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
